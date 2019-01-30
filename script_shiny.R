8c67c303d8c66a03baa21160fd098e8f2c26cab3
rebecacarvalho <40205325+rebecacarvalho@users.noreply.github.com>
2019-01-30 13:55:09 -0200

    .

---
+++ script_shiny.R
@@ -0,0 +1,91 @@
+
+
+# Shiny dos indicadores CepespData
+# Autor: Rebeca Carvalho
+
+rm(list = ls())
+
+# Pacotes utilizados
+
+library(cepespR)
+library(knitr)
+library(tidyverse)
+library(lubridate)
+library(shiny)
+library(plyr)
+library(shinyalert)
+library(shinyBS)
+library(ggplot2)
+library(shiny)
+
+
+
+# 1. User interface -------------------------------------------------------
+
+
+ui <- fluidPage(
+  
+  # Titulo do app ----
+  
+  titlePanel("Cepesp Indicadores"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput(inputId = "DESCRICAO_CARGO",
+                  label = "Escolha um cargo:",
+                  choices = c("Deputado Federal", "Deputado Estadual", "Vereador"),
+                  selected = "Deputado Federal"),
+      
+      selectInput(inputId = "AGREGACAO_REGIONAL",
+                  label = "Escolha uma agregação regional:",
+                  choices = c("Brasil", "UF", "Municipio"),
+                  selected = "Brasil"),
+      
+      selectInput(inputId = "ANO_ELEICAO",
+                  label = "Escolha um ano:",
+                  choices = c(1998,2002,2006,2010,2014,2018),
+                  selected = 1998),
+      
+      selectInput(inputId = "SIGLA_UE",
+                  label = "Escolha um Estado:",
+                  choices = c("AC","AM","AL","AP","BA","CE","DF","ES","GO","MA",
+                              "MS","MG","MT","PA","PB","PE","PI","PR","RJ","RN",
+                              "RO","RR","RS","SC","SE","SP","TO"),
+                  selected = "AC")
+    ),
+    
+    
+    mainPanel(
+      plotOutput("graph")
+    )
+  )
+)
+
+
+
+# 2. Server ---------------------------------------------------------------
+
+
+
+write.csv(df, "df.csv")
+
+server <- function(input, output) {
+  
+  output$graph <- renderPlot({
+    ggplot(df, mapping = aes(x = input$ANO_ELEICAO, y = input$DESCRICAO_CARGO)) +
+      geom_bar(stat = "identity")
+  }
+  
+  
+  
+  )
+  
+}
+
+
+# 3. ShinyApp -------------------------------------------------------------
+
+
+shinyApp(ui = ui, server = server)
