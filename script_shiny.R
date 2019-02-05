4f3cb45031c8691bd33a19d929cb7811d1bb4596
rebecacarvalho <40205325+rebecacarvalho@users.noreply.github.com>
2019-02-05 13:37:06 -0200

    new

--- script_shiny.R
+++ script_shiny.R
@@ -28,73 +28,124 @@ df <- read.csv("df.csv")
 
 de <- read.csv("de.csv")
 
-# 1. User interface -------------------------------------------------------
+# 2. User interface -------------------------------------------------------
 
 
 ui <- fluidPage(
   
-  # Titulo do app ----
-  
- navbarPage("CepespIndicadores"),
+ navbarPage("CepespIndicadores", theme = shinytheme("flatly"),
  
- tabPanel("Indicadores de Fragmentação Legislativa"),
+ tabPanel("Fragmentação legislativa", theme = shinytheme("flatly")),
+ 
+ tabPanel("Renovação das bancadas"),
+ 
+ tabPanel("Alienação"),
+ 
+ tabPanel("Sobre")),
  
  absolutePanel(top = 60, left = 10, right = "auto", bottom = "auto",
                width = 260, height = "auto", draggable = FALSE, fixed = TRUE,
                h4("Opções:"),
       selectInput(inputId = "DESCRICAO_CARGO",
-                  label = "Escolha um cargo:",
+                  label = "Escolha um cargo",
                   choices = c("Deputado Federal", "Deputado Estadual", "Vereador"),
                   selected = "Deputado Federal"),
       
-      selectInput(inputId = "ANO_ELEICAO",
-                  label = "Escolha um ano:",
-                  choices = c(1998,2002,2006,2010,2014,2018),
-                  selected = 1998),
+      uiOutput("ANO_EST"),
       
-      selectInput(inputId = "SIGLA_UE",
-                  label = "Escolha um Estado:",
-                  choices = c("AC","AM","AL","AP","BA","CE","DF","ES","GO","MA",
-                              "MS","MG","MT","PA","PB","PE","PI","PR","RJ","RN",
-                              "RO","RR","RS","SC","SE","SP","TO"),
-                  selected = "AC"),
+      uiOutput("ANO_MUN"),
+      
+      uiOutput("AGREG_EST"),
+      
+      uiOutput("AGREG_MUN"),
       
       selectInput("INDICADORES",
-                  label = "Escolha um indicador:",
+                  label = "Escolha um indicador",
                   choices = c("Desproporcionalidade de Gallagher", "Fracionalização", "Fracionalização máxima",
                               "Fragmentação", "Número efetivo de partidos", "Quociente eleitoral"),
                   selected = "Desproporcionalidade de Gallagher")
     
-    ),
+    ))
+
+
+
+
+# 3. Server ---------------------------------------------------------------
+
+
+
+  server <- function(input, output,session){
+  
+# Ano da eleicao
+  
+  ano <- reactive({
+    cargo <- input$DESCRICAO_CARGO
+    if(cargo == "Vereador"){
+    return(ouput$ANO_MUN)
+  } else {
+    return(output$ANO_EST)
+  }
     
-        mainPanel(
-      plotOutput("graph")
-    )
-  )
-
-
-
-
-# 2. Server ---------------------------------------------------------------
-
-
-
-server <- function(input, output,session) {
+  })
   
+  output$ANO_MUN <- renderUI({
+    cargo <- input$DESCRICAO_CARGO
+    if(cargo == "Vereador"){
+    selectizeInput("ANO_MUN", 
+                    label = "Escolha um ano",
+                    choices = c(2000,2004,2008,2012,2016),
+                    selected = 2000)
+  }
+    
+  })
+     
+  output$ANO_EST <- renderUI({
+    cargo <- input$DESCRICAO_CARGO
+    if(cargo %in% c("Deputado Federal", "Deputado Estadual")){
+    selectizeInput("ANO_EST",
+                   label = "Escolha um ano",
+                   choices = c(1998,2002,2006,2010,2014),
+                   selected = 1998)
+  }
+  })
   
-  output$text <- renderPlot({
-    ggplot(df, mapping = aes(x = input$ANO_ELEICAO, y = df$VOTOS_VALIDOS/513)) +
-      geom_density()
+
+# Agregacao regional
+
+  agreg <- reactive({
+   cargo <- input$DESCRICAO_CARGO
+   if(cargo == "Vereador"){
+   return(input$AGREG_MUN)
+  } else {
+   return(output$AGREG_EST)
+  }
+  })
+  
+  output$AGREG_MUN <- renderUI({
+    cargo <- input$DESCRICAO_CARGO
+    if(cargo == "Vereador"){
+    selectizeInput("AGREG_MUN",
+                   label = "Escolha um município",
+                   choices = )  
+  }
+  })
+  
+  output$AGREG_EST <- renderUI({
+    cargo <- input$DESCRICAO_CARGO
+    if(cargo %in% c("Deputado Federal", "Deputado Estadual")){
+    selectizeInput("AGREG_EST",
+                   label = "Escolha um estado",
+                   choices = c("AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG",
+                   "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC",
+                   "SE", "SP", "TO"),
+                   selected = "AC")  
+  }
+  })
   }
   
-  
-  
-  )
-  
-}
 
 
-# 3. ShinyApp -------------------------------------------------------------
+# 4. ShinyApp -------------------------------------------------------------
 
 
 shinyApp(ui = ui, server = server)
